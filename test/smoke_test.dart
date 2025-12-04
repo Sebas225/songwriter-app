610a271d21289f03b0be481790416fa90ab30ce9
Sebas225 <80087540+Sebas225@users.noreply.github.com>
2025-12-04 09:47:49 -0500

    Initialize Flutter project skeleton

---
+++ test/smoke_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:songwriter/app.dart';
+
+void main() {
+  testWidgets('renders home page', (tester) async {
+    await tester.pumpWidget(const ProviderScope(child: SongwriterApp()));
+
+    await tester.pumpAndSettle();
+
+    expect(find.text('Home'), findsOneWidget);
+  });
+}
